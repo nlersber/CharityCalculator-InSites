bb752ff401a60827fc189ce8171924b8fa0c8ffb
Nick <nicklersberghe@gmail.com>
2021-06-11 20:38:17 +0200

    Add test (which is kinda unnecessary though)

---
+++ Backend/CharityCalculator/CharityTest/Models/TaxRateTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CharityCalculator.Controllers;
+using CharityCalculator.Domain.IServices;
+using CharityCalculator.Domain.Models;
+using CharityCalculator.Domain.ServiceInstances;
+using Moq;
+using Xunit;
+
+namespace CharityTest.Controllers
+{
+    public class TaxRateTest
+    {
+
+        private TaxRate rate;
+
+        public TaxRateTest()
+        {
+            rate = new TaxRate { Rate = 20D };
+        }
+
+        [Fact]
+        public void TestRate()
+        {
+            Assert.Equal(25D, rate.CalculateDonationDeductibleAmount(new Donation { Amount = 100 }));
+        }
+
+
+
+    }
+}
